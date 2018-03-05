4a4e334622d2560c5362ab4141aee62a13117eb8
falinwa <benoit@sbg-live.com>
2018-03-05 22:06:25 +0800

    added composeapps script

--- Readme.md
+++ Readme.md
@@ -1,7 +1,22 @@
 # List of docker-compose ready apps
 Using docker and docker-compose is not so easy for the beginners... This is a repository of the apps I use, hopefully it can save time to others !
 
-## Shadowsocks
+## How to quickly use on your server ?
+If you want to easily install on your server/computer, you can use a small batch script (UNIX only) as described below.
+Replace shadowsocks by the folder name of the app name you wish to use.
+```bash
+wget -O - https://raw.githubusercontent.com/sbglive/compose/master/composapps.sh
+./composeapps.sh shadowsocks
+cd shadowsocks
+docker-compose up
+```
+
+## Make it your own ? Contribute
+Feel free to fork, clone or contribute !
+
+
+# APP List
+## shadowsocks
 Based on mritd/shadowsocks:latest
 - Make sure to open firewall ports are 6443/tcp and 6500/udp on your host
 - Change password "test123" in the .yml file
---
+++ composapps.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# DEFINE SOME HELPERS FUNCTIONS TO MAKE OUR SCRIPT PRETTY
+#
+# DEFINE LOG FUNCTION
+DATE='date +%Y/%m/%d-%H:%M:%S'
+
+function _intro {
+    echo " "
+    echo "================================"
+    echo "SBG COMPOSE > $1"
+    echo "--------------------------------"
+    echo "Benoit Lavorata, 2018"
+    echo "================================"
+    echo " "
+}
+function _info {
+    echo `$DATE`"| $1"
+}
+function _error {
+    echo `$DATE`"| [ERROR] $1"
+    echo " "
+}
+function _success {
+    echo `$DATE`"| [SUCCESS] $1"
+    echo " "
+}
+function _section {
+    echo " "
+    echo `$DATE`"| --- $1 ---"
+}
+function _quit_if_not_root {
+    echo " "
+    #CHECK THAT WE ARE ROOT
+    _info "Log in as root"
+    if [[ $EUID -ne 0 ]]; then
+        _error "You must be root user (try sudo)"
+        exit 1
+    fi
+    _success "Log in as root"
+}
+function _url_exists {
+     echo " "
+}
+
+#
+# WHERE IT ALL HAPPENS
+#
+
+#Title
+_intro "Get app $1"
+
+#Check if url exists
+URL="https://raw.githubusercontent.com/sbglive/compose/master/$1/docker-compose.yml"
+_info "Check if app exists on repository: $1"
+if curl --output /dev/null --silent --head --fail "$URL"; then
+    _success "OK, the app exists"
+else
+    _fail "The app $1 does not exist on the repository (did you spell it properly ?)"
+    exit 1
+fi
+
+#Create folder
+_info "Create folder: $1"
+mkdir $1
+cd $1
+_success "OK, folder $1 created"
+
+#Download script
+_info "Download script: $URL"
+wget -O - $URL
+_success "OK, script downloaded at $1/docker-compose.yml"
+
+_section "Thank you for using the composapps script ! See you soon"
