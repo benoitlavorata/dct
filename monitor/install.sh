7f2523a4883b3debc070c7e9c2b38c46b0dd523a
falinwa <benoit@sbg-live.com>
2018-05-04 19:35:50 +0800

    update

--- monitor/install.sh
+++ monitor/install.sh
@@ -1,6 +1,14 @@
-_section "Installation script"
-git clone https://github.com/sbglive/dockprom.git
-mv dockprom/* ../
+#!/bin/bash
 
+#VARIABLES
+CUSTOM_GIT_FOLDER="dockprom"
+CUSTOM_GIT_URL="https://github.com/sbglive/$CUSTOM_GIT_FOLDER.git"
+
+_section "Clone $CUSTOM_GIT_URL"
+git clone https://github.com/sbglive/dockprom.git
+_success "Cloned repository"
+
+
+_section "Build start scripts"
 # ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d
 
