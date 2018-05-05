bcb1eedd4e0ba4c3da46124bbcfd8b6ebaf15563
falinwa <benoit@sbg-live.com>
2018-05-05 23:33:05 +0800

    update

--- _ubuntu/install.sh
+++ _ubuntu/install.sh
@@ -23,8 +23,19 @@ _break_line
 _log "Install npm packages (global)"
 sudo npm install -g $NPM_LIST
 _break_line
-
 _success "Installed"
+
+_log "Install compose.sh as bin in path"
+cd ~/ 
+mkdir bin
+cp ~/.bash_profile ~/.bash_profile.compose.backup
+echo 'PATH=$PATH:$HOME/bin' >> ~/.bash_profile 
+cd ~/bin
+rm compose.sh
+wget https://raw.githubusercontent.com/sbglive/compose/master/compose.sh
+chmod +x compose.sh
+mv compose.sh app
+cd "$SCRIPT_WORKING_DIR_PATH/$APP_NAME"
 
 _section "Remove $APP_NAME files"
 cd .. 
