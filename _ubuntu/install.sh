4ab5eacb72192933c1800829e8c090320e07aee6
falinwa <benoit@sbg-live.com>
2018-05-05 23:23:10 +0800

    update

--- _ubuntu/install.sh
+++ _ubuntu/install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-_section "Remove old versions"
-sudo apt-get remove docker docker-engine docker.io
-_success "removed old versions"
+PACKAGE_LIST="vim docker docker-compose gcc build-essential nodejs npm wget curl"
+NPM_LIST="forever n"
 
-_section "Install"
+_section "Install usual packages (personal use)"
+_log "Ubuntu packages: ${PACKAGE_LIST}"
+_log "NPM packages: ${NPM_LIST}"
+_break_line
+
+_log "Update"
 sudo apt-get update
-sudo apt-get install docker docker-compose -y
+
+_log "Upgrade"
+sudo apt-get upgrade -y
+
+_log "Install ubuntu packages"
+sudo apt-get install $PACKAGE_LIST
+
+_log "Install npm packages (global)"
+sudo apt-get install $PACKAGE_LIST
+
 _success "Installed"
 
-_section "Remove install files"
+_section "Remove $APP_NAME files"
 cd .. 
-rm -r _docker
-_success "removed files"
-
+rm -r $APP_NAME
+_success "Removed files"
