69b5c1cfb06427fcbf454c2db014e49bf6982e29
falinwa <benoit@sbg-live.com>
2018-05-17 23:55:11 +0800

    update

--- _apt_mirrors/install.sh
+++ _apt_mirrors/install.sh
@@ -5,7 +5,9 @@ _log "Backup original sources.list"
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 _log "Change sources.list"
-sudo sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list
+_download "https://raw.githubusercontent.com/sbglive/compose/master/$APP_NAME/sources.list"
+mv sources.list /etc/apt/sources.list
+#sudo sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list
 
 _log "Update APT (be patient)"
 apt-get update
